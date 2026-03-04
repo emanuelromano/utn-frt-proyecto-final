e854e29a094e2440aa83f6313628bb4baa2a3fe4
emanuelromano <eromanox@gmail.com>
2026-03-04 14:34:52 -0300

    Actualizadas URL de imagenes en script

--- database/pasteleria.sql
+++ database/pasteleria.sql
@@ -37,15 +37,15 @@ PRIMARY KEY (`id`)
 );
 
 insert into productos (nombre, url, imagen, descripcion, porciones, precio, enCarro, cantidadCompra)
-values ("Selva Negra", "selva-negra", "https://i.ibb.co/5vwq3XY/1.jpg", "Bizcochuelo de chocolate empapado en kirsch e intercaladas con nata y cerezas.", 8, 5500, false, 0),
-("Lemon Pie", "lemon-pie", "https://i.ibb.co/vv4gQFF/2.jpg", "Tarta formada por una base de masa quebrada u hojaldre rellena con crema de limón.", 8, 2600, false, 0),
-("Torta Rogel", "torta-rogel", "https://i.ibb.co/HNGQR7y/3.jpg", "Capas de discos de masa Rogel crocante, relleno de dulce de leche con chips de chocolate y terminado con merengue italiano.", 8, 3500, false, 0),
-("Red Velvet", "red-velvet", "https://i.ibb.co/8D5CnJw/4.jpg", "Bizcocho de color rojo en capas, relleno con crema y con un glaseado de crema de queso.", 8, 6000, false, 0),
-("Carrot Cake", "carrot-cake", "https://i.ibb.co/vHDYBBV/5.jpg", "Budín de zanahoria, con una pizca de canela, cubierto con un frosting de queso crema dulce y topping de almendras tostadas.", 8, 4800, false, 0),
-("Cheese Cake", "cheese-cake", "https://i.ibb.co/xMH7rXv/6.jpg", "Base crocante de galletas y manteca, crema de queso y limón combinada con una perfecta confitura casera de frambuesa.", 8, 5000, false, 0),
-("Chocotorta", "chocotorta", "https://i.ibb.co/jzsFLFr/7.jpg", "Siete capas de Chocolinas intercaladas con crema de dulce de leche repostero.", 8, 4700, false, 0),
-("Torta Matcha", "torta-matcha", "https://i.ibb.co/yQhrhPW/8.jpg", "Una suave y delicada torta de té verde (matcha) y un mousse de chocolate blanco.", 8, 6600, false, 0),
-("Torta de Chocolate y Licor", "torta-de-chocolate-y-licor", "https://i.ibb.co/c1jKHqB/9.jpg", "Tres capas de bizcocho de chocolate unidas por de un delicioso merengue de licor de whisky.", 8, 7000, false, 0);
+values ("Selva Negra", "selva-negra", "https://raw.githubusercontent.com/emanuelromano/utn-frt-proyecto-final/refs/heads/main/img/img-productos-backup/1.jpg", "Bizcochuelo de chocolate empapado en kirsch e intercaladas con nata y cerezas.", 8, 5500, false, 0),
+("Lemon Pie", "lemon-pie", "https://raw.githubusercontent.com/emanuelromano/utn-frt-proyecto-final/refs/heads/main/img/img-productos-backup/2.jpg", "Tarta formada por una base de masa quebrada u hojaldre rellena con crema de limón.", 8, 2600, false, 0),
+("Torta Rogel", "torta-rogel", "https://raw.githubusercontent.com/emanuelromano/utn-frt-proyecto-final/refs/heads/main/img/img-productos-backup/3.jpg", "Capas de discos de masa Rogel crocante, relleno de dulce de leche con chips de chocolate y terminado con merengue italiano.", 8, 3500, false, 0),
+("Red Velvet", "red-velvet", "https://raw.githubusercontent.com/emanuelromano/utn-frt-proyecto-final/refs/heads/main/img/img-productos-backup/4.jpg", "Bizcocho de color rojo en capas, relleno con crema y con un glaseado de crema de queso.", 8, 6000, false, 0),
+("Carrot Cake", "carrot-cake", "https://raw.githubusercontent.com/emanuelromano/utn-frt-proyecto-final/refs/heads/main/img/img-productos-backup/5.jpg", "Budín de zanahoria, con una pizca de canela, cubierto con un frosting de queso crema dulce y topping de almendras tostadas.", 8, 4800, false, 0),
+("Cheese Cake", "cheese-cake", "https://raw.githubusercontent.com/emanuelromano/utn-frt-proyecto-final/refs/heads/main/img/img-productos-backup/6.jpg", "Base crocante de galletas y manteca, crema de queso y limón combinada con una perfecta confitura casera de frambuesa.", 8, 5000, false, 0),
+("Chocotorta", "chocotorta", "https://raw.githubusercontent.com/emanuelromano/utn-frt-proyecto-final/refs/heads/main/img/img-productos-backup/7.jpg", "Siete capas de Chocolinas intercaladas con crema de dulce de leche repostero.", 8, 4700, false, 0),
+("Torta Matcha", "torta-matcha", "https://raw.githubusercontent.com/emanuelromano/utn-frt-proyecto-final/refs/heads/main/img/img-productos-backup/8.jpg", "Una suave y delicada torta de té verde (matcha) y un mousse de chocolate blanco.", 8, 6600, false, 0),
+("Torta de Chocolate y Licor", "torta-de-chocolate-y-licor", "https://raw.githubusercontent.com/emanuelromano/utn-frt-proyecto-final/refs/heads/main/img/img-productos-backup/9.jpg", "Tres capas de bizcocho de chocolate unidas por de un delicioso merengue de licor de whisky.", 8, 7000, false, 0);
 
 create table cupones (
 id int auto_increment not null,
